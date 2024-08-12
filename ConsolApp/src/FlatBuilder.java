d6170ef98d58f602dd59d837a8766ea92f897e5d
MacBook <macbook@MacBook-Pro-MacBook.local>
2024-08-12 15:46:27 +0300

    Changed class FlatBuilder

---
+++ ConsolApp/src/FlatBuilder.java
@@ -0,0 +1,181 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FlatBuilder {
+    public static final int MIN_AREA = 0;
+    public static final int MAX_AREA = 858;
+
+    public static Flat build() {
+
+        String name = "default";
+        Coordinates coordinates = new Coordinates(0d, 0f);
+        int area = 1; //Максимальное значение поля: 858, Значение поля должно быть больше 0
+        Long numberOfRooms = 1L; //Значение поля должно быть больше 0
+        double price = 1; //Значение поля должно быть больше 0
+        Boolean furniture = null; //Поле может быть null
+        View view = View.PARK; //Поле не может быть null
+        House house = null; //Поле может быть null
+
+        name = readName();
+        coordinates = readCoordinates();
+        area = readArea();
+        numberOfRooms = readNumberOfRooms();
+        price = readPrice();
+        furniture = readFurniture();
+        view = readView();
+        house = readHouse();
+
+        return new Flat(name, coordinates, area, numberOfRooms, price, furniture, view, house);
+    }
+
+
+    public static Flat flatUpdate(Flat flat) {
+        flat.setName(readName());
+        flat.setCoordinates(readCoordinates());
+        flat.setArea(readArea());
+        flat.setNumberOfRooms(readNumberOfRooms());
+        flat.setPrice(readPrice());
+        flat.setFurniture(readFurniture());
+        flat.setView(readView());
+        flat.setHouse(readHouse());
+
+        return flat;
+    }
+
+    public static String readName() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter name:");
+                String input = scanner.nextLine();
+                if (input.isEmpty()) {
+                    throw new WrongInputException("Name couldn't be null");
+                }
+                return input;
+            } catch (WrongInputException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Coordinates readCoordinates() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter coordinates x and y:");
+                String input = scanner.nextLine();
+                String[] args = input.split(" ");
+                if (args.length != 2) {
+                    throw new WrongInputException("Coordinates consists of 2 numbers");
+                }
+                return new Coordinates(Double.valueOf(args[0]), Float.valueOf(args[1]));
+            } catch (WrongInputException | NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static int readArea() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter area:");
+                int input = Integer.valueOf(scanner.nextLine());
+                if (input <= MAX_AREA && input > MIN_AREA) {
+                    return input;
+                } else {
+                    throw new WrongInputException("Area must be in diapason (0, 858]");
+                }
+            } catch (WrongInputException | NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Long readNumberOfRooms() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter number of rooms:");
+                Long numberOfRooms = Long.valueOf(scanner.nextLine());
+                return numberOfRooms;
+            } catch (NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Double readPrice() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter price:");
+                Double price = Double.parseDouble(scanner.nextLine());
+                return price;
+            } catch (WrongInputException | NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Boolean readFurniture() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter furniture:");
+                String input = scanner.nextLine();
+                if (input.equals("yes") || input.equals("да")) {
+                    return (true);
+                } else {
+                    if (input.equals("no") || input.equals("нет")) {
+                        return (false);
+                    } else {
+                        throw new WrongInputException("Enter [yes/no/да/нет]");
+                    }
+                }
+            } catch (WrongInputException | NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static View readView() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter view:" + Arrays.toString(View.values()));
+                String input = scanner.nextLine();
+                if (input.isEmpty()) {
+                    throw new WrongInputException("Enter a number of view");
+                }
+                int number = Integer.valueOf(input);
+                for (View i : View.values()) {
+                    if (i.ordinal() == number) {
+                        return i;
+                    }
+                }
+            } catch (WrongInputException | NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+    public static House readHouse(){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Printer.print("Enter house - [name, year, number of flats on floor]:");
+                String input = scanner.nextLine();
+                String[] args = input.split(" ");
+                if (args.length != 3) {
+                    throw new WrongInputException("[name, year, number of flats on floor]");
+                }
+                return new House(args[0], Long.valueOf(args[1]), Long.valueOf(args[2]));
+            } catch (WrongInputException | NumberFormatException e) {
+                Printer.println(e.getMessage());
+            }
+        }
+    }
+
+
+}
