7d28598fba77e7d36cef0b708cffa6e05b8f43ec
Daria Koroleva <macbook@MacBook-Pro-MacBook.local>
2024-08-17 00:34:50 +0300

    added message about the result in receiver

--- ConsolApp/src/pattern/Invoker.java
+++ ConsolApp/src/pattern/Invoker.java
@@ -1,5 +1,7 @@
+package pattern;
+
 import commands.*;
-import pattern.Receiver;
+import tools.Response;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,18 +15,26 @@ public class Invoker {
         setCommand("info", new Info(receiver));
         setCommand("show", new Show(receiver));
         setCommand("add", new Add(receiver));
-        setCommand("updateById", new UpdateById(receiver));
-        setCommand("removeById", new RemoveById(receiver));
+        setCommand("update_by_id", new UpdateById(receiver));
+        setCommand("remove_by_id", new RemoveById(receiver));
         setCommand("clear", new Clear(receiver));
-        setCommand("addIfMax", new AddIfMax(receiver));
+        setCommand("add_if_max", new AddIfMax(receiver));
+        setCommand("remove_greater", new RemoveGreater(receiver));
+        setCommand("remove_lower", new RemoveLower(receiver));
+        setCommand("count_greater_than_house", new CountGreaterThanHouse(receiver));
+        setCommand("filter_by_furniture", new FilterByFurniture(receiver));
     }
 
     public void setCommand(String key, Command command) {
         commands.put(key, command);
     }
 
-    public void executeCommand(String key, String arg) {
-        commands.get(key).execute(arg);
+    public Response executeCommand(String key, String arg) {
+        try {
+            return commands.get(key).execute(arg);
+        } catch (NullPointerException e) {
+            return new Response(e.getMessage());
+        }
     }
 
 }
--- ConsolApp/src/pattern/Receiver.java
+++ ConsolApp/src/pattern/Receiver.java
@@ -1,5 +1,7 @@
-import data.Flat;
-import data.House;
+package pattern;
+
+import data.*;
+import tools.Response;
 
 import java.util.ArrayDeque;
 import java.util.Date;
@@ -10,63 +12,67 @@ public class Receiver {
     private Date creationDate = new Date();
     private Flat maxElement;
 
-    public Receiver() {
-    }
+    public Receiver() {}
 
-    public void add(Flat flat) {
+    public Response add(Flat flat) {
         collection.addLast(flat);
         if (maxElement == null || flat.compareTo(maxElement) > 0) {
             maxElement = flat;
         }
+        return new Response("The element " + flat + " has been added");
     }
 
-    public void help() {
-        Printer.print("available operations:" +
+    public Response help() {
+        return new Response("available operations:\n" +
                 "help : info about available operations\n" +
                 "info : information about collection\n" +
                 "show : see all the items in the collection\n" +
                 "add {element} : add new element to collection\n" +
-                "updateId {element} : update the value of a collection item whose ID is equal to the specified one\n" +
-                "removeById id : remove an item from the collection by its id\n" +
+                "update_id {element} : update the value of a collection item whose ID is equal to the specified one\n" +
+                "remove_by_id id : remove an item from the collection by its id\n" +
                 "clear : clear the collection\n" +
                 "save : save the collection to a file\n" +
                 "execute_script file_name : Read and execute the script from the specified file. The script contains commands in the same form as they are entered by the user interactively.\n" +
                 "exit : end the program (without saving to a file)\n" +
-                "addIfMax {element} : add a new element to the collection if its value exceeds the value of the largest element of this collection\n" +
-                "removeGreater {element} : remove from the collection all elements exceeding the specified one\n" +
-                "removeLower {element} : remove from the collection all elements smaller than the specified\n" +
-                "countGreaterThanHouse house : print the number of elements whose house field value is greater than the specified one\n" +
-                "filterByFurniture furniture : output elements whose value of the furniture field is equal to the specified\n" +
-                "printAscending : output the elements of the collection in ascending order"
+                "add_if_max {element} : add a new element to the collection if its value exceeds the value of the largest element of this collection\n" +
+                "remove_greater {element} : remove from the collection all elements exceeding the specified one\n" +
+                "remove_lower {element} : remove from the collection all elements smaller than the specified\n" +
+                "count_greater_than_house house : print the number of elements whose house field value is greater than the specified one\n" +
+                "filter_by_furniture furniture : output elements whose value of the furniture field is equal to the specified\n" +
+                "print_ascending : output the elements of the collection in ascending order"
         );
     }
 
-    public void info() {
-        Printer.print("Elements: ");
-        Printer.printCollection(collection);
-        Printer.println(
-                "Date: " + creationDate + "\n" +
-                        "Size: " + collection.size() + "\n" +
-                        "Max Element: " + maxElement
-        );
+    public Response info() {
+        StringBuilder result = new StringBuilder("Elements: ");
+        result.append("Date: ").append(creationDate).append("\n");
+        result.append("Size: ").append(collection.size()).append("\n");
+        result.append("Max Element: ").append(maxElement);
+        return new Response(String.valueOf(result));
     }
 
-    public void show() {
+    public Response show() {
         if (collection.isEmpty()) {
-            Printer.println("Collection is empty");
+            return new Response("Collection is empty");
         } else {
-            Printer.print("[");
+            StringBuilder result = new StringBuilder("[ ");
             for (Flat flat : collection) {
-                Printer.print(flat + ", ");
+                result.append(flat.toString()).append(", ");
             }
-            Printer.print("]");
+            result.deleteCharAt(result.length()-2);
+            result.append("]");
+            return new Response(String.valueOf(result));
         }
     }
 
-    public void removeById(Long id) {
+    public Response removeById(Long id) {
+        Response result = new Response("Collection not contains element flat" + id);;
         for (Flat flat : collection) {
             if (flat.getId().equals(id)) {
-                collection.remove(flat);
+                if (collection.remove(flat)) {
+                    result = new Response("Element was deleted successfully");
+                }
+                break;
             }
         }
 
@@ -79,67 +85,81 @@ public class Receiver {
             }
         }
 
+        return result;
     }
 
-    public void updateById (Long id){
+    public Response updateById (Long id){
+        Response result = new Response("Collection not contains element flat" + id);
         for (Flat flat : collection) {
             if (flat.getId().equals(id)) {
-                 flat = FlatBuilder.flatUpdate(flat);
+                 FlatReader.flatUpdate(flat);
+                 result = new Response("flat " + id + " was updated");
             }
+            break;
         }
+        return result;
     }
 
-    public void clear() {
-        for (Flat flat : collection) {
-            collection.remove(flat);
+    public Response clear() {
+        while (!collection.isEmpty()) {
+            collection.remove();
         }
+        return new Response("The collection has been cleared");
     }
 
-    public void addIfMax(Flat flat) {
+    public Response addIfMax(Flat flat) {
         if (flat.compareTo(maxElement) > 0) {
-            collection.add(flat);
             maxElement = flat;
+            return add(flat);
         }
+        return new Response("The element is less than the maximum");
     }
 
-    public void removeGreater(Flat flat) {
+    public Response removeGreater(Flat flat) {
+        Response result = new Response("Collection not contains element greater than" + flat);
         for (Flat iter : collection) {
             if (iter.compareTo(flat) > 0) {
                 collection.remove(iter);
+                result = new Response("Elements was deleted");
             }
         }
+        return result;
     }
 
-    public void removeLower(Flat flat) {
+    public Response removeLower(Flat flat) {
+        Response result = new Response("Collection not contains element lower than" + flat);
         for (Flat iter : collection) {
             if (iter.compareTo(flat) < 0) {
                 collection.remove(iter);
+                result = new Response("Elements was deleted");
             }
         }
+        return result;
     }
 
-    public void countGreaterThanHouse(House house) {
+    public Response countGreaterThanHouse(House house) {
         int counter = 0;
         for (Flat iter : collection) {
             if (iter.getHouse().compareTo(house) > 0) {
                 counter++;
             }
         }
-        Printer.println("Count greater than house '" + house + "' = " + counter);
+        return new Response("Count greater than house '" + house + "' = " + counter);
     }
 
-    public void filterByFurniture(Boolean furniture) {
-        Printer.println("Filter by furniture '" + furniture + "':");
+    public Response filterByFurniture(Boolean furniture) {
+        StringBuilder result = new StringBuilder("Filter by furniture '" + furniture + "':");
         int counter = 1;
         for (Flat flat : collection) {
             if (flat.getFurniture() == furniture) {
-                Printer.println(counter + ". " + flat);
+                result.append(counter + ". " + flat + "\n");
                 counter++;
             }
         }
         if (counter == 1) {
-            Printer.println("There is no flats");
+            result.append("There is no flats");
         }
+        return new Response(String.valueOf(result));
     }
 
 }
